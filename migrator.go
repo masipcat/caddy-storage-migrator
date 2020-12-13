c395cd6b331e68bb0680053e805d40e988ac778d
Jordi Masip <jordi@masip.cat>
2020-12-13 14:10:47 +0100

    Initial version

---
+++ migrator.go
@@ -0,0 +1,124 @@
+package migrator
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"unsafe"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/certmagic"
+
+	redis "github.com/gamalan/caddy-tlsredis"
+)
+
+var storages = map[string]func() certmagic.Storage{
+	"redis": func() certmagic.Storage {
+		return new(redis.RedisStorage)
+	},
+}
+
+func InitStorage(name string, config []byte) (interface{}, error) {
+	newStorage, found := storages[name]
+	if !found {
+		return nil, fmt.Errorf("'%s' not found", name)
+	}
+	storage := newStorage().(interface{})
+
+	if len(config) > 0 {
+		err := json.Unmarshal(config, storage)
+		if err != nil {
+			return nil, fmt.Errorf("Couldn't unmarshal config: %v", err)
+		}
+	}
+
+	if prov, ok := storage.(caddy.Provisioner); ok {
+		ctx, _ := caddy.NewContext(caddy.Context{Context: context.Background()})
+		cfg := &caddy.Config{Logging: new(caddy.Logging)}
+		// Hack to set a Config to caddy.Context
+		// https://stackoverflow.com/a/43918797
+		rs := reflect.ValueOf(&ctx).Elem()
+		rf := rs.Field(2) // ctx.cfg
+		ri := reflect.ValueOf(&cfg).Elem()
+		rf = reflect.NewAt(rf.Type(), unsafe.Pointer(rf.UnsafeAddr())).Elem()
+		rf.Set(ri)
+		ri.Set(rf)
+
+		err := prov.Provision(ctx)
+		if err != nil {
+			return nil, fmt.Errorf("Error during Provision: %s", err)
+		}
+	}
+
+	if vali, ok := storage.(caddy.Validator); ok {
+		err := vali.Validate()
+		if err != nil {
+			return nil, fmt.Errorf("Error during Validate: %s", err)
+		}
+	}
+
+	return storage, nil
+}
+
+func ImportFiles(storage certmagic.Storage, caddyFolder string) error {
+	caddyFolder, err := filepath.Abs(caddyFolder)
+	if err != nil {
+		return err
+	}
+	paths := make([]string, 0)
+	err = filepath.Walk(caddyFolder, func(fullpath string, f os.FileInfo, err error) error {
+		if err != nil {
+			log.Printf("Error ocurred in %s: %v", fullpath, err)
+		}
+		if !f.IsDir() {
+			paths = append(paths, fullpath)
+		}
+		return nil
+	})
+
+	for _, fullpath := range paths {
+		path := strings.TrimPrefix(fullpath, caddyFolder)
+		fmt.Printf("Importing %s...\n", path)
+		binary, err := ioutil.ReadFile(fullpath)
+		if err != nil {
+			return err
+		}
+		err = storage.Store(path, binary)
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
+func ExportFiles(storage certmagic.Storage, dest string) error {
+	dest, err := filepath.Abs(dest)
+	if err != nil {
+		return err
+	}
+	keys, err := storage.List("*", true)
+	if err != nil {
+		return err
+	}
+	for _, key := range keys {
+		fmt.Printf("Exporting %s...\n", key)
+		val, err := storage.Load(key)
+		if err != nil {
+			return err
+		}
+		path := filepath.Join(dest, key)
+		err = os.MkdirAll(filepath.Dir(path), 0700)
+		if err != nil {
+			return err
+		}
+		ioutil.WriteFile(path, val, 0600)
+	}
+	return nil
+}
---
+++ migrator_test.go
@@ -0,0 +1,184 @@
+package migrator
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
+	"testing"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/certmagic"
+)
+
+type DummyStorage struct {
+	stored      map[string][]byte
+	lock        *sync.Mutex
+	Value       string `json:"value"`
+	provisioned bool
+	validated   bool
+}
+
+func (ds *DummyStorage) Store(key string, value []byte) error {
+	ds.stored[key] = value
+	return nil
+}
+
+func (ds *DummyStorage) Load(key string) ([]byte, error) {
+	val, found := ds.stored[key]
+	if found {
+		return val, nil
+	}
+	return nil, errors.New("Key not found")
+}
+
+func (ds *DummyStorage) Delete(key string) error {
+	delete(ds.stored, key)
+	return nil
+}
+
+func (ds *DummyStorage) Exists(key string) bool {
+	_, found := ds.stored[key]
+	return found
+}
+
+func (ds *DummyStorage) List(prefix string, recursive bool) ([]string, error) {
+	keys := make([]string, 0, len(ds.stored))
+	if prefix == "*" {
+		for k := range ds.stored {
+			keys = append(keys, k)
+		}
+	}
+	return keys, nil
+}
+
+func (ds *DummyStorage) Stat(key string) (certmagic.KeyInfo, error) {
+	_, found := ds.stored[key]
+	if found {
+		return certmagic.KeyInfo{Key: key, IsTerminal: true}, nil
+	}
+	return certmagic.KeyInfo{Key: key}, errors.New("Key not found")
+}
+
+func (ds *DummyStorage) Lock(ctx context.Context, key string) error {
+	ds.lock.Lock()
+	return nil
+}
+
+func (ds *DummyStorage) Unlock(key string) error {
+	ds.lock.Unlock()
+	return nil
+}
+
+func (ds *DummyStorage) Provision(ctx caddy.Context) error {
+	ds.provisioned = true
+	return nil
+}
+
+func (ds *DummyStorage) Validate() error {
+	ds.validated = true
+	return nil
+}
+
+// Interface guards
+var (
+	_ certmagic.Storage = (*DummyStorage)(nil)
+	_ caddy.Provisioner = (*DummyStorage)(nil)
+	_ caddy.Validator   = (*DummyStorage)(nil)
+)
+
+func TestImport(t *testing.T) {
+	storages["dummy"] = func() certmagic.Storage {
+		return &DummyStorage{stored: map[string][]byte{}}
+	}
+
+	config := []byte("{\"value\": \"test json unmarshal\"}")
+
+	s, err := InitStorage("dummy", config)
+	if err != nil {
+		t.Error(err)
+	}
+	ds := s.(*DummyStorage)
+
+	if ds.Value != "test json unmarshal" {
+		t.Failed()
+	}
+
+	if ds.provisioned == false {
+		t.Error("Function Provision() not called")
+	}
+
+	if ds.validated == false {
+		t.Error("Function Validate() not called")
+	}
+
+	caddyPath, err := ioutil.TempDir(".", "test-")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(caddyPath)
+	f, err := ioutil.TempFile(caddyPath, "key-")
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	f.Write([]byte("aaaa"))
+	err = f.Sync()
+	if err != nil {
+		t.Error(err)
+	}
+	err = ImportFiles(ds, caddyPath)
+	if err != nil {
+		t.Error(err)
+	}
+	if len(ds.stored) == 0 {
+		t.Error("File not stored")
+	}
+}
+
+func TestExport(t *testing.T) {
+	fileContent := []byte("THIS IS A CERT")
+	storages["dummy"] = func() certmagic.Storage {
+		return &DummyStorage{stored: map[string][]byte{
+			"cert1": fileContent,
+		}}
+	}
+
+	config := []byte{}
+
+	_, err := InitStorage("dumy", config)
+	if err == nil {
+		t.Errorf("Should fail. Invalid storage name")
+	}
+
+	s, err := InitStorage("dummy", config)
+	if err != nil {
+		t.Error(err)
+	}
+	ds := s.(*DummyStorage)
+
+	dest, err := ioutil.TempDir(".", "test-")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(dest)
+	ExportFiles(ds, dest)
+
+	exported := make([]string, 0)
+	filepath.Walk(dest, func(path string, info os.FileInfo, err error) error {
+		exported = append(exported, path)
+		return nil
+	})
+
+	if len(exported) == 0 {
+		t.Errorf("Nothing exported")
+	}
+
+	data, err := ioutil.ReadFile(filepath.Join(dest, "cert1"))
+	if !bytes.Equal(data, fileContent) {
+		t.Errorf("File content mismatch")
+	}
+}
